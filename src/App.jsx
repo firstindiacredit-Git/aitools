84de1e2164e8011659f8c16df313fba807ce00ce
rizwan3749 <aarishidrisi3749x@gmail.com>
2025-11-11 11:25:46 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 function App() {
   return (
     <main className="main-content fade-in">
-      <h1>Toolz Access</h1>
-      <p>Welcome! Customize `src/App.jsx` to build your experience.</p>
+      
+      <script type="module" src="home.html"></script>
     </main>
   )
 }
